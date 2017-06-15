78d4523ccb5cae738b048e8c66765fc2d7daefc5
Usuario <Usuario>
2017-06-15 11:11:33 -0500

    se agregó la función para agregar usuarios

--- coneccion.py
+++ coneccion.py
@@ -10,4 +10,9 @@ def mostrar_usuario():
     for fila in r:
         print(fila)
         
+def insertar_usuario(datos):
+    c.execute("INSERT INTO usuario(nombre, apellido1, apellido2, correo, password, tipo) VALUES(?,?,?,?,?,?)", (datos[0], datos[1], datos[2], datos[3], datos[4], datos[5]))
+    db.commit()
+        
+insertar_usuario(["r", "gda", "r", "@", "p", 1])
 mostrar_usuario()
